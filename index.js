eb68d2fe77b27c884bc8f0fd1754d94b8c816c4b
Saleem Saleh <salem1234saleem@gmail.com>
2020-08-06 15:10:55 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function BoardMember (name, homeState,training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+}
+
+BoardMember.prototype.veto  = function() {
+  return "No, I must disagree" ;
+};
+
+BoardMember.prototype.doCharity   = function() {
+  return "I like to help people." ;
+};
+
+BoardMember.prototype.approve   = function() {
+  return "You can do that!" ;
+};
